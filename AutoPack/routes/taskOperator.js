98243f68119963a038a91dae38e26bed6d155052
Ruiqi-Alipay <ruiqi.sg@gmail.com>
2015-03-12 14:25:16 +0800

    BUG

--- AutoPack/routes/taskOperator.js
+++ AutoPack/routes/taskOperator.js
@@ -1,6 +1,7 @@
 var fs = require('fs')
 var path = require('path');
 var Task = require('../modules/task.js');
+var exec = require('child_process').exec;
 
 module.exports = {
 	taskId: function (req, res, next, taskId) {
