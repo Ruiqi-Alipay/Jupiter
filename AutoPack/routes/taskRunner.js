f8d349ad92229040aa429a6bc21f91bb68fffabd
Ruiqi-Alipay <ruiqi.sg@gmail.com>
2015-03-19 20:08:14 +0800

    AUTOPACK BUG

--- AutoPack/routes/taskRunner.js
+++ AutoPack/routes/taskRunner.js
@@ -7,6 +7,7 @@ var exec = require('child_process').exec;
 var channel = require('./channel.js');
 var path = require('path');
 var qr = require('qr-image');
+var Regex = require("regex");
 
 // 	"args": {
 // 		"dir.spec": "$SVN_PATH",
@@ -62,101 +63,100 @@ var runTask = function (project, task, action) {
 	var defer = q.defer();
 
 	try {
-		// var child = exec('svn log -l 1 ' + project.svn, function (error, stdout, stderr) {
-		// 	var versionMessage = stdout;
+		var dir = path.join(__dirname, '..', 'Projects', task.project);
 
-		// 	var rePattern = new RegExp(/r\d*/);
-		// 	var arrMatches = versionMessage.match(rePattern);
-		// 	var version = arrMatches[0].slice(1);
+		channel.emit(task._id, 'Deleting old svn files...: ');
+		var child = exec('sudo rm -R ' + dir, function (error, stdout, stderr) {
 
-		// 	var child = exec('svn diff -c ' + version + ' ' + project.svn + ' --summarize', function(error, stdout, stderr) {
-		// 		versionMessage += ('\r\n' + stdout);
+			channel.emit(task._id, 'SVN checking out: ' + project.svn);
+			var child = exec('svn checkout --username ' + project.username + ' --password ' + project.password
+					+ ' ' + project.svn + ' ' + dir, {
+					maxBuffer: 200*1024*1024
+				}, function (error, stdout, stderr){
 
-				channel.emit(task._id, 'Preparing for task: ' + task.name);
-				var dir = path.join(__dirname, '..', 'Projects', task.project);
+				channel.emit(task._id, 'SVN sync complate! checking svn current reversion...');
+				var child = exec('svn log -l 1 ' + project.svn, function (error, stdout, stderr) {
 
-				var child = exec('sudo rm -R ' + dir, function (error, stdout, stderr) {
-					channel.emit(task._id, 'SVN checking out: ' + project.svn);
-					var command = 'svn checkout --username ' + project.username + ' --password ' + project.password
-							+ ' ' + project.svn + ' ' + dir;
-					var child = exec(command, {
-							maxBuffer: 200*1024*1024
-						}, function (error, stdout, stderr){
-							channel.emit(task._id, 'SVN checking complate! preparing to build project...');
+					var versionMessage = stdout;
+					var arrMatches = versionMessage.match(new Regex(/r\d*/));
+					var version = arrMatches[0].slice(1);
 
-							var args = makeArgs(dir, action);
-							var jarPath = path.join(dir, project.packPath);
-							var saveDir = path.join(__dirname, '..', 'download', task._id.toString());
-							if (!fs.existsSync(saveDir)) {
-								fs.mkdirSync(saveDir);
-							}
-							var child = exec('java -Dfile.encoding=UTF-8' + args + ' -jar pack.jar', {
-										cwd: jarPath,
-										maxBuffer: 200*1024*1024
-									}, function (error, stdout, stderr){
-										if (error) console.log(error);
-										if (stderr) console.log(stderr);
+					channel.emit(task._id, 'SVN current version is: ' + version + '; starting build process...');
+					var child = exec('svn diff -c ' + version + ' ' + project.svn + ' --summarize', function(error, stdout, stderr) {
+						versionMessage += ('\r\n' + stdout);
 
-										var result = path.join(__dirname, '..', 'Projects', task.project, project.packPath, 'result.json');
-										if (fs.existsSync(result)) {
-											var result = JSON.parse(fs.readFileSync(result));
-											if (result && result.result && result.pkgs) {
-												var fse = require('fs-extra');
-												var downlaodRecord = [];
-												result.pkgs.forEach(function (pkgPath) {
-													var fileName = pkgPath.slice(pkgPath.lastIndexOf('/') + 1);
-													fse.copySync(path.join(dir, pkgPath), path.join(saveDir, fileName));
-													
-													var link = 'http://autotest.d10970aqcn.alipay.net/autopack/download/' + task._id.toString() + '/' + fileName;
+						var args = makeArgs(dir, action);
+						var jarPath = path.join(dir, project.packPath);
+						var saveDir = path.join(__dirname, '..', 'download', task._id.toString());
+						if (!fs.existsSync(saveDir)) {
+							fs.mkdirSync(saveDir);
+						}
+						var child = exec('java -Dfile.encoding=UTF-8' + args + ' -jar pack.jar', {
+									cwd: jarPath,
+									maxBuffer: 200*1024*1024
+								}, function (error, stdout, stderr){
+									if (error) console.log(error);
+									if (stderr) console.log(stderr);
 
-													var code = qr.image(link, { type: 'png' });
-													var output = fs.createWriteStream(path.join(saveDir, fileName + '.png'));
-													code.pipe(output);
+									var result = path.join(__dirname, '..', 'Projects', task.project, project.packPath, 'result.json');
+									if (fs.existsSync(result)) {
+										var result = JSON.parse(fs.readFileSync(result));
+										if (result && result.result && result.pkgs) {
+											var fse = require('fs-extra');
+											var downlaodRecord = [];
+											result.pkgs.forEach(function (pkgPath) {
+												var fileName = pkgPath.slice(pkgPath.lastIndexOf('/') + 1);
+												fse.copySync(path.join(dir, pkgPath), path.join(saveDir, fileName));
+												
+												var link = 'http://autotest.d10970aqcn.alipay.net/autopack/download/' + task._id.toString() + '/' + fileName;
 
-													downlaodRecord.push({
-														name: fileName,
-														link: link
-													});
+												var code = qr.image(link, { type: 'png' });
+												var output = fs.createWriteStream(path.join(saveDir, fileName + '.png'));
+												code.pipe(output);
+
+												downlaodRecord.push({
+													name: fileName,
+													link: link
 												});
+											});
 
-												// task.svnStat = versionMessage;
-												task.downloads = JSON.stringify(downlaodRecord);
-												defer.resolve(task);
-											} else {
-												defer.reject(new Error('Pack failed!'));
-											}
+											task.svnStat = versionMessage;
+											task.downloads = JSON.stringify(downlaodRecord);
+											defer.resolve(task);
 										} else {
-											defer.reject(new Error('Result file not found!'));
+											defer.reject(new Error('Pack failed!'));
 										}
+									} else {
+										defer.reject(new Error('Result file not found!'));
+									}
 
-										channel.emit(task._id, '*** Build execution ' + (true ? 'finished! ***' : 'failed! ***'));
-									});
+									channel.emit(task._id, '*** Build execution ' + (true ? 'finished! ***' : 'failed! ***'));
+								});
 
-							child.stdout.on('data', function (data) {
-								channel.emit(task._id, data);
-							});
-
-							child.stdout.pipe(fs.createWriteStream(path.join(saveDir, 'record.log')));
-
-							updateRunningState(task, child);
+						child.stdout.on('data', function (data) {
+							channel.emit(task._id, data);
 						});
 
-					child.stdout.on('data', function (data) {
-						channel.emit(task._id, data);
+						child.stdout.pipe(fs.createWriteStream(path.join(saveDir, 'record.log')));
+
+						updateRunningState(task, child);
 					});
 
 					updateRunningState(task, child);
 				});
 
 				updateRunningState(task, child);
-				channel.emit(task.project, 'run-task-start');
-		// 	});
+			});
 
-		// 	updateRunningState(task, child);
-		// });
+			child.stdout.on('data', function (data) {
+				channel.emit(task._id, data);
+			});
 
-		// updateRunningState(task, child);
-		// channel.emit(task.project, 'run-task-start');
+			updateRunningState(task, child);
+		});
+
+		updateRunningState(task, child);
+		channel.emit(task.project, 'run-task-start');
 	} catch (err) {
 		console.log(err);
 
