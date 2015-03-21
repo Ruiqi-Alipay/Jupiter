9d632d3c02aae8dba60e784116148237781f6919
Ruiqi-Alipay <ruiqi.sg@gmail.com>
2015-03-21 18:02:57 +0800

    Rich text

--- YoNote/webapp/app/scripts/app.js
+++ YoNote/webapp/app/scripts/app.js
@@ -1,6 +1,12 @@
-(function (document) {
-  'use strict';
+var editor = new wysihtml5.Editor("wysihtml5-editor", {
+	toolbar: "wysihtml5-editor-toolbar",
+	parserRules: wysihtml5ParserRules
+});
 
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
-})(wrap(document));
+editor.on("load", function() {
+	var composer = editor.composer,
+	    h1 = editor.composer.element.querySelector("h1");
+	if (h1) {
+	  composer.selection.selectNode(h1);
+	}
+});
